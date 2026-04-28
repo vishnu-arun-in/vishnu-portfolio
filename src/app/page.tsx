7909818ccae0a525aac84122adcd2233ce78254f
Vishnu Arun <vishnuarun071@gmail.com>
2026-04-28 22:09:10 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-export default function ExperienceSection() {
+export default function SkillsSection() {
   return (
     <section style={{ padding: "40px", color: "white" }}>
-      <h2>Experience</h2>
+      <h2>Skills</h2>
       <ul>
-        <li>Epixel Solutions – QA Engineer (2023–Present)</li>
-        <li>QAWebPrints – Software Test Engineer (2021–2023)</li>
+        <li>Manual Testing</li>
+        <li>API Testing (Postman)</li>
+        <li>Selenium (Basic)</li>
+        <li>Jira, Redmine</li>
+        <li>SQL (Basic)</li>
       </ul>
     </section>
   );
