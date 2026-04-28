df77073323e3a00e2c66dbd60cfd28dd39592e8e
Vishnu Arun <vishnuarun071@gmail.com>
2026-04-28 19:02:59 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-import React from 'react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import HeroSection from '@/app/components/HeroSection';
-import AboutSection from '@/app/components/AboutSection';
-import ExperienceSection from '@/app/components/ExperienceSection';
-import SkillsSection from '@/app/components/SkillsSection';
-import ProjectsSection from '@/app/components/ProjectsSection';
-import CertificationsSection from '@/app/components/CertificationsSection';
-import ContactSection from '@/app/components/ContactSection';
-
-export default function HomePage() {
-  return (
-    <main className="relative overflow-x-hidden">
-      <div className="noise-overlay" />
-      <Header />
-      <HeroSection />
-      <AboutSection />
-      <ExperienceSection />
-      <SkillsSection />
-      <ProjectsSection />
-      <CertificationsSection />
-      <ContactSection />
-      <Footer />
-    </main>
-  );
-}
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import ExperienceSection from './components/ExperienceSection';
+import SkillsSection from './components/SkillsSection';
+import ProjectsSection from './components/ProjectsSection';
+import CertificationsSection from './components/CertificationsSection';
+import ContactSection from './components/ContactSection';
