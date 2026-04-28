bf2f99741e6a43c38dda9a1bfbe8ed404414dec0
Vishnu Arun <vishnuarun071@gmail.com>
2026-04-28 19:19:53 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HeroSection from './components/HeroSection';
-import AboutSection from './components/AboutSection';
-import ExperienceSection from './components/ExperienceSection';
-import SkillsSection from './components/SkillsSection';
-import ProjectsSection from './components/ProjectsSection';
-import CertificationsSection from './components/CertificationsSection';
-import ContactSection from './components/ContactSection';
+export default function Home() {
+  return (
+    <div>
+      <h1>Site Working ✅</h1>
+    </div>
+  );
+}
