34b1bb176a6d98ad0c24c027a79873c92747a0dc
Vishnu Arun <vishnuarun071@gmail.com>
2026-05-10 17:27:12 +0530

    Update ContactSection.tsx

--- src/app/components/ContactSection.tsx
+++ src/app/components/ContactSection.tsx
@@ -38,7 +38,7 @@ const socialLinks = [
     icon: 'CodeBracketSquareIcon',
     label: 'GitHub',
     href: 'https://github.com',
-    display: 'github.com/vishnuarun',
+    display: 'https://github.com/vishnu-arun-in',
   },
 ];
 
